82f648ef69062fabc5900e3700e110459ed6bbc8
Rachel Grier <rgrier104@gmail.com>
2018-08-16 02:22:58 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+end
